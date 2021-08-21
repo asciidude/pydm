8062536c5081af9244111cdec579895633835cf1
James Barr <sickspamemail@gmail.com>
2021-08-20 23:37:51 -0500

    README.md patch, add to .gitignore, src/pydm.py

--- src/pydm.py
+++ src/pydm.py
@@ -2,14 +2,16 @@ import requests
 import traceback
 import os
 
-# Testing
-if not os.path.isdir(f'{os.path.abspath(__file__)}/dl'):
-    os.makedirs(f'{os.path.abspath(__file__)}/dl', exist_ok=True)
+gui = False
 
-url = requests.get('htstps://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf')
+if gui == False:
+    url = requests.get('htstps://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf')
+    # url = requests.get(input('Input the DDL of your file: '))
 
-try:
-    with open('dummy_file.pdf', 'wb') as f:
-        f.write(url.content)
-except:
-    print(f'Failed to download file, error: {traceback.print_tb}')
+    try:
+        with open(f'{os.path.abspath(__file__)}/dummy_file.pdf', 'wb') as f:
+            f.write(url.content)
+    except:
+        print("Unable to connect to server or download file from server")
+else:
+    pass
