98e25a01b7c0079ec29c7f43759c3e9fd78ed528
James Barr <sickspamemail@gmail.com>
2021-08-21 01:03:56 -0500

    complete CLI

--- src/pydm.py
+++ src/pydm.py
@@ -1,22 +1,49 @@
-import requests
-from clint.textui import progress
 import os
+import requests
+import argparse
+import utils.str2bool
+from clint.textui import progress
 
-gui = False
-req = None #requests.get('link', stream=True)
+parser = argparse.ArgumentParser(description="A download manager")
+
+parser.add_argument(
+    "gui",
+    nargs=1,
+    metavar="gui",
+    type=utils.str2bool.str2bool,
+    help="Set if you want to use the GUI or not",
+)
+
+parser.add_argument(
+    "link",
+    nargs=1,
+    metavar="link",
+    type=str,
+    help="Set the DDL link where the file will be downloaded",
+)
+
+parser.add_argument(
+    "name",
+    nargs=1,
+    metavar="name",
+    type=str,
+    help="Set the name and extension of the file",
+)
+
+args = parser.parse_args()
 
 # CLI
-if gui is False:
-    # res = requests.get(input('Input the DDL of your file: '))
-    file = input("What would you like your file to be named (example.js)? ")
+if args.gui[0] is False:
+    if not os.path.isdir(f"{os.getcwd()}/dl"):
+        os.makedirs(f"{os.getcwd()}/dl")
 
-    if not os.path.isdir(f'{os.getcwd()}/dl'):
-        os.makedirs(f'{os.getcwd()}/dl')
+    req = requests.get(args.link[0], stream=True)
 
-    with open(f'{os.getcwd()}/dl/{file}', 'wb') as f:
-        print(f"Downloading file {file}")
+    with open(f"{os.getcwd()}/dl/{args.name[0]}", "wb") as f:
+        print(f"Downloading file {args.name[0]}")
+
         try:
-            total_length = int(req.headers.get('content-length'))
+            total_length = int(req.headers.get("content-length"))
         except requests.exceptions.InvalidHeader:
             print("[CONTACT DEVELOPER] Invalid header has been provided to download")
 
@@ -24,13 +51,15 @@ if gui is False:
             print("Unable to connect to server or download file from server")
         else:
             try:
-                for chunk in progress.bar(req.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1): 
+                for chunk in progress.bar(
+                    req.iter_content(chunk_size=1024),
+                    expected_size=(total_length / 1024) + 1,
+                ):
                     if chunk:
                         f.write(chunk)
                         f.flush()
             except requests.exceptions.InvalidURL:
                 print("URL provided has been invalidated by the requests module")
-
 # GUI
 else:
     pass
